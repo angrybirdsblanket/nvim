974f44005a29d448f5ba43407f43dd1f199510ba
Ivan <ivan.v.dochev@gmail.com>
2025-03-03 20:46:20 +0800

    integrated terminal done

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,10 +1,35 @@
+-- lua/terminal.lua
 return {
-  'rebelot/terminal.nvim',
+  "akinsho/toggleterm.nvim",
   config = function()
-    require("terminal").setup()
-    vim.keymap.set("n", "<C-`>", function()
-      require("terminal").toggle()
-    end, { noremap = true, silent = true })
-  end
+    require("toggleterm").setup({
+      direction = "horizontal",
+      -- Set terminal size based on window dimensions
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return math.floor(vim.o.columns * 0.4)
+        else
+          return math.floor(vim.o.lines * 0.8)
+        end
+      end,
+      open_mapping = [[<C-`>]], -- Shortcut to toggle the terminal
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "curved",
+        width = math.floor(vim.o.columns * 0.8),
+        height = math.floor(vim.o.lines * 0.8),
+        winblend = 3,
+      },
+    })
+    -- Create a normal-mode key mapping to toggle the terminal
+    vim.keymap.set("n", "<C-`>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+  end,
 }
 
