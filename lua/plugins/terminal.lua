e89af62681e4814c3f56cff87bb171e95bb1aff8
Ivan <ivan.v.dochev@gmail.com>
2025-03-03 20:36:19 +0800

    added toggle on and off shortcut for integrated terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,6 +1,10 @@
 return {
-    'rebelot/terminal.nvim',
-    config = function()
-        require("terminal").setup()
-    end
+  'rebelot/terminal.nvim',
+  config = function()
+    require("terminal").setup()
+    vim.keymap.set("n", "<C-`>", function()
+      require("terminal").toggle()
+    end, { noremap = true, silent = true })
+  end
 }
+
