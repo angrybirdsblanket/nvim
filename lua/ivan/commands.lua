f1d85929fb7cfb575f185dcf1c311f8b6a5c23a2
Ivan <ivan.v.dochev@gmail.com>
2025-06-11 14:43:18 +0800

    actually fixed it this time

--- lua/ivan/commands.lua
+++ lua/ivan/commands.lua
@@ -8,12 +8,22 @@ vim.api.nvim_create_user_command("Prettier", function()
     json              = true,
   }
   if supported[filetype] then
-    local file_path = vim.fn.expand("%:.")
+    -- Store the cursor position before formatting
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local file_path = vim.fn.shellescape(vim.fn.expand("%:."))
     local shell     = vim.o.shell
-    local command   = { shell, "-ic", "command format " .. file_path }
+    -- Modified command to properly quote and escape the filepath
+    local command   = { shell, "-ic", "format " .. file_path }
     local result    = vim.fn.system(command)
+    
     if vim.v.shell_error ~= 0 then
       vim.notify("Format failed:\n" .. result, vim.log.levels.ERROR)
+    else
+      -- Reload the buffer after successful formatting
+      vim.cmd("edit!")
+      -- Restore cursor position
+      pcall(vim.api.nvim_win_set_cursor, 0, cursor_pos)
+      vim.notify("Format successful", vim.log.levels.INFO)
     end
   else
     vim.notify("Prettier is not supported for this file type: " .. filetype, vim.log.levels.WARN)
